6119f97ea8749c2ca5af12369f49fe6a78b1998a
Chris <christopher_m_mitchell@live.co.uk>
2025-06-02 21:23:48 +0100

    [FEAT]
    Added init command to cli

--- transmute/main.py
+++ transmute/main.py
@@ -1,9 +1,11 @@
 import typer
 
 from transmute.scaffold import scaffold_app
+from transmute.init import init_app
 
 app = typer.Typer()
 app.add_typer(scaffold_app, name="scaffold")
+app.add_typer(init_app, name="init")
 
 if __name__ == "__main__":
     app()
